d381d618ac42d6d8c10fd972ff153167dbd06cb9
marcusturewicz <24448509+marcusturewicz@users.noreply.github.com>
2020-01-14 10:54:50 +1100

    Add twitter code

--- src/DotNetTwitterBot/Function.cs
+++ src/DotNetTwitterBot/Function.cs
@@ -1,5 +1,7 @@
 using System;
 using Amazon.Lambda.Core;
+using Tweetinvi;
+using Tweetinvi.Parameters;
 
 namespace DotNetTwitterBot
 {
@@ -11,7 +13,38 @@ namespace DotNetTwitterBot
 
         public void Retweet(ILambdaContext context)
         {
-            context.Logger.LogLine($"Function executed at {DateTime.UtcNow}\n");
+            context.Logger.LogLine($"Function executed at {DateTime.UtcNow}");
+
+            Auth.SetUserCredentials(
+                Environment.GetEnvironmentVariable("TWITTER_CONSUMER_KEY"),
+                Environment.GetEnvironmentVariable("TWITTER_CONSUMER_SECRET_KEY"),
+                Environment.GetEnvironmentVariable("TWITTER_ACCESS_TOKEN"),
+                Environment.GetEnvironmentVariable("TWITTER_ACCESS_TOKEN_SECRET"));
+
+            var searchTerms = new[] { ".NET Framework", ".NET Core", "dotnet", "dotnetcore", ".NET 5" };
+
+            var searchSince = DateTime.UtcNow.Subtract(TimeSpan.FromMinutes(30));
+            
+            var me = User.GetAuthenticatedUser();
+
+            foreach (var term in searchTerms)
+            {
+                var param = new SearchTweetsParameters(term) { Since = searchSince };
+                var tweets = Search.SearchTweets(param);
+                foreach (var tweet in tweets)
+                {
+                    try
+                    {
+                        Tweet.PublishRetweet(tweet.Id);
+                        me.FollowUser(tweet.CreatedBy.Id);
+                    }
+                    catch (Exception e)
+                    {
+                        context.Logger.LogLine($"Exception occured: {e.Message}");
+                    }
+                }
+            }
         }
     }
 }
+
