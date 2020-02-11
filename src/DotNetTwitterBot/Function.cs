3770634c5fd2768b7cd67a9aa61106c4ddcc88e7
Marcus Turewicz <24448509+marcusturewicz@users.noreply.github.com>
2020-02-11 22:10:01 +1100

    Add search term for myself

--- src/DotNetTwitterBot/Function.cs
+++ src/DotNetTwitterBot/Function.cs
@@ -20,7 +20,7 @@ namespace DotNetTwitterBot
 
             Auth.SetUserCredentials(creds.ConsumerKey, creds.ConsumerSecret, creds.AccessToken, creds.AccessSecret);
 
-            var searchTerms = new[] { "\".NET Framework\"", "\".NET Core\"", "\".NET 5\"", "dotnet", "dotnetcore" };
+            var searchTerms = new[] { "\".NET Framework\"", "\".NET Core\"", "\".NET 5\"", "dotnet", "dotnetcore", "_dotnetbot_" };
 
             var searchSince = DateTime.UtcNow.Subtract(TimeSpan.FromMinutes(10));
 
