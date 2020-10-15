a2e4b44f7dc66566e8186adf3e1e44190177cb81
Marcus Turewicz <24448509+marcusturewicz@users.noreply.github.com>
2020-10-16 10:47:59 +1100

    Update to TweetInviAPI 5 (#25)

--- src/DotNetTwitterBot/Function.cs
+++ src/DotNetTwitterBot/Function.cs
@@ -5,6 +5,7 @@ using Amazon.Lambda.Core;
 using Tweetinvi;
 using Tweetinvi.Models;
 using Tweetinvi.Parameters;
+using Tweetinvi.Parameters.Enum;
 
 namespace DotNetTwitterBot
 {
@@ -14,7 +15,8 @@ namespace DotNetTwitterBot
         {
             "\".NET Framework\"",
             "\".NET Core\"",
-            "\".NET 5\""
+            "\".NET 5\"",
+            "\".NET 6\""
         };
 
         static readonly string[] SearchTracks = new[]
@@ -28,27 +30,27 @@ namespace DotNetTwitterBot
         public async Task Retweet(ILambdaContext context)
         {
             var creds = await SecretHelper.GetSecretAsync();
-            Auth.SetUserCredentials(creds.ConsumerKey, creds.ConsumerSecret, creds.AccessToken, creds.AccessSecret);
+            var client = new TwitterClient(creds.ConsumerKey, creds.ConsumerSecret, creds.AccessToken, creds.AccessSecret);
             
             var searchSince = DateTime.UtcNow.Subtract(TimeSpan.FromMinutes(35));
-            var me = User.GetAuthenticatedUser();
+            var me = await client.Users.GetAuthenticatedUserAsync();
 
             await Task.WhenAll(
-                SearchAndRetweetTweets(SearchTerms, searchSince, me),
-                SearchAndRetweetTweets(SearchTracks, searchSince, me));
+                SearchAndRetweetTweets(client, SearchTerms, searchSince, me),
+                SearchAndRetweetTweets(client, SearchTracks, searchSince, me));
 
-            static async Task SearchAndRetweetTweets(string[] terms, DateTime searchSince, IAuthenticatedUser me)
+            static async Task SearchAndRetweetTweets(TwitterClient client,string[] terms, DateTime searchSince, IAuthenticatedUser me)
             {
                 var filterTerms = new[] { "domain", "registration", "domainregistration", "@paul_dotnet" };
                 var query = string.Join(" OR ", terms);
                 var param = new SearchTweetsParameters(query)
                 {
                     Since = searchSince,
-                    TweetSearchType = TweetSearchType.OriginalTweetsOnly,
                     Filters = TweetSearchFilters.Safe
                 };
 
-                var tweets = await SearchAsync.SearchTweets(param);
+                var tweets = await client.Search.SearchTweetsAsync(param);
+                tweets = client.Search.FilterTweets(tweets, OnlyGetTweetsThatAre.OriginalTweets, false);
                 foreach (var tweet in tweets)
                 {
                     // Exclude tweets that contain excluded words.
